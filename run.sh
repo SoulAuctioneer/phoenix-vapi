487e0150611a825fc2e2be07d7f1562586d35450
SoulAuctioneer <ash.eldritch@gmail.com>
2025-01-31 19:20:13 -0800

    don't change dir

--- run.sh
+++ run.sh
@@ -3,5 +3,5 @@ if [ -z "$VIRTUAL_ENV" ]; then
     source venv/bin/activate
 fi
 export PYTHONPATH="${PYTHONPATH}:$(pwd)/src"
-cd src && sudo python3 main.py
+sudo python3 main.py
 echo "Phoenix App Exited"
