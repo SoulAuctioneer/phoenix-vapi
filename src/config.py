3021143572eb2f69a8b4adb421a6fede8725fbbe
SoulAuctioneer <ash.eldritch@gmail.com>
2025-02-15 14:59:08 -0800

    switcheroo

--- src/config.py
+++ src/config.py
@@ -184,9 +184,8 @@ class BLEConfig:
 
     # Known beacon locations using (major, minor) tuples as keys
     BEACON_LOCATIONS = {
-        # TODO: Switch 'em around
-        (1, 1): "pendant", # Label: Phoenix_Library
-        (1, 2): "blue_phoenix" # Label: Phoenix_Bedroom
+        (1, 1): "blue_phoenix", # Label: Phoenix_Library
+        (1, 2): "pendant" # Label: Phoenix_Bedroom
     }
 
     # RSSI thresholds for distance estimation (in dB)
