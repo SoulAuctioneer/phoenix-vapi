16769ad9b045cab6854bd298c5b34affb0bfed83
SoulAuctioneer <ash.eldritch@gmail.com>
2025-03-11 16:51:16 -0700

    increase LED count

--- src/config.py
+++ src/config.py
@@ -53,7 +53,7 @@ class WakeWordConfig:
 
 # LED Configuration
 LED_PIN = 21  # GPIO10 for NeoPixel data - Using this to keep audio enabled on GPIO18
-LED_COUNT = 24  # Number of NeoPixels in the ring
+LED_COUNT = 50  # Number of NeoPixels in the ring
 LED_BRIGHTNESS = 1.0  # LED brightness (0.0 to 1.0)
 LED_ORDER = "GRB"  # Color order of the LEDs (typically GRB or RGB)
 
