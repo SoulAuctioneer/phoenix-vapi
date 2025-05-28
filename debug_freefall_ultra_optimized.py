f6c8e65164cfcb05c4c78c661666a24c336984cf
Ash <ash.eldritch@gmail.com>
2025-05-27 19:55:16 -0700

    grr

--- debug_freefall_ultra_optimized.py
+++ debug_freefall_ultra_optimized.py
@@ -26,7 +26,7 @@ hardware_logger.setLevel(logging.WARNING)
 
 # Enable debug logging for the accelerometer manager to see state transitions
 accel_logger = logging.getLogger('managers.accelerometer_manager')
-accel_logger.setLevel(logging.DEBUG)
+accel_logger.setLevel(logging.INFO)
 
 async def debug_freefall_ultra_optimized():
     """Ultra-optimized debug function with only essential sensors."""
